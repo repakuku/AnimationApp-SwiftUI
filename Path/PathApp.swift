8dc283579790555449c49fef4e182ec033942070
Alexey Turulin <repakuku@icloud.com>
2023-09-13 17:34:23 -0400

    Initial Commit

---
+++ Path/PathApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathApp.swift
+//  Path
+//
+//  Created by Alexey Turulin on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct PathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
